0563c00ba92953506a30a9cd95a1c4c825dfffb1
UmarlyPoeta <pkozlowski@student.agh.edu.pl>
2024-07-03 06:30:47 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+import os
+import sys
+
 
 server_adress = ("localhost", 4221)
 
@@ -8,7 +11,6 @@ def handling_responses(client_socket, request):
     # decoding request and spliting based on the \r\n
     status_line,*args = request.decode().split("\r\n")
     
-    print(status_line)
     try:
         method, path, protocol = status_line.split(" ")
     except Exception as e:
@@ -16,6 +18,32 @@ def handling_responses(client_socket, request):
         return
     
     
+    if path.startswith("/files/"):
+        try:
+            command_line_arguments = sys.argv[1:]
+            
+            if "--directory" not in command_line_arguments:
+                raise Exception
+            else:
+                path_directory_index = command_line_arguments.index("--directory")+1
+        except:
+            client_socket.send("HTTP/1.1 400 Bad Request\r\n\r\n".encode("utf-8"))
+            return
+        
+        string_from_request = path.split("/")[-1]
+        
+        file_path = f"{command_line_arguments[path_directory_index]}{string_from_request}"
+        
+        if os.path.isfile(file_path):
+            file = open(file_path,"rb")
+            content = file.read().decode("utf-8")
+            
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(content)}\r\n\r\n{content}"
+            client_socket.send(response.encode("utf-8"))
+        else:
+            client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8"))
+        
+        
     if path == "/user-agent":
         user_agent=args[1]
         string_from_request = user_agent.split(" ")[-1]
@@ -38,7 +66,7 @@ def handling_responses(client_socket, request):
         client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8"))
         
 
-def client_thread(connection):
+def client_thread(connection,client_address):
     try:
         while True:
             request = connection.recv(1024)
@@ -58,8 +86,7 @@ def main():
     try:
         while True:
             connection, client_address = server_socket.accept() # wait for client
-            threading.Thread(target=client_thread,args=(connection,)).start()
-            
+            threading.Thread(target=client_thread,args=(connection,client_address)).start()
     finally:
         server_socket.close()
 
