216eae57611c375498613635f586e67a46f57e35
UmarlyPoeta <pkozlowski@student.agh.edu.pl>
2024-07-03 06:48:36 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def handling_responses(client_socket, request):
     if path.startswith("/files/"):
         try:
             command_line_arguments = sys.argv[1:]
-            
+
             if "--directory" not in command_line_arguments:
                 raise Exception
             else:
@@ -29,22 +29,37 @@ def handling_responses(client_socket, request):
         except:
             client_socket.send("HTTP/1.1 400 Bad Request\r\n\r\n".encode("utf-8"))
             return
+
+        string_from_path = path.split("/")[-1]
+
+        file_path = f"{command_line_arguments[path_directory_index]}{string_from_path}"
         
-        string_from_request = path.split("/")[-1]
+        match method:
+            case "GET":
+                if os.path.isfile(file_path):
+                    file = open(file_path,"rb")
+                    content = file.read().decode("utf-8")
+
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(content)}\r\n\r\n{content}"
+                    client_socket.send(response.encode("utf-8"))
+                else:
+                    client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8"))
+                
+            case "POST":
+                if os.path.isdir(command_line_arguments[path_directory_index]):
+                    body_request = args[-1]
+                    with open(file_path,"w") as file:
+                        file.write(body_request)
+                    client_socket.send("HTTP/1.1 201 Created\r\n\r\n".encode("utf-8"))
+                else:
+                    client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8"))   
+                    
+            case _:
+                client_socket.send("HTTP/1.1 400 Bad Request\r\n\r\n".encode("utf-8"))
+                return
         
-        file_path = f"{command_line_arguments[path_directory_index]}{string_from_request}"
         
-        if os.path.isfile(file_path):
-            file = open(file_path,"rb")
-            content = file.read().decode("utf-8")
-            
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(content)}\r\n\r\n{content}"
-            client_socket.send(response.encode("utf-8"))
-        else:
-            client_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8"))
-        
-        
-    if path == "/user-agent":
+    elif path == "/user-agent":
         user_agent=args[1]
         string_from_request = user_agent.split(" ")[-1]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string_from_request)}\r\n\r\n{string_from_request}"
@@ -52,7 +67,7 @@ def handling_responses(client_socket, request):
     
     
     
-    if path.startswith("/echo/"): # /echo/{str} endpoint
+    elif path.startswith("/echo/"): # /echo/{str} endpoint
         string_from_request = path.split("/")[-1]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string_from_request)}\r\n\r\n{string_from_request}"
         client_socket.send(response.encode("utf-8"))
